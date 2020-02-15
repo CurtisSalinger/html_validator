32e828b46acf3f31f81a2048d6c7130189ff473b
Curtis Salinger <csalinger22@cmc.edu>
2020-02-14 18:22:37 -0800

    HTML_Val basic 10

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -30,7 +30,7 @@ def validate_html(html):
 
     tags = _extract_tags(html)
     while (len(tags) % 2 == 0):
-        if tags[1][1:] not in tags[-1][2:]:
+        if tags[0][1:] not in tags[-1][2:]:
             return False
         else:
             tags = tags[2:-1]
