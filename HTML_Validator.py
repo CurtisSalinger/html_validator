787fa8c70263a9e6b5100426e6dea2aea4b4658e
Curtis Salinger <csalinger22@cmc.edu>
2020-02-14 17:29:22 -0800

    HTML_Val basic

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -9,6 +9,7 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
+    """
     lst = []
     for tag in _extract_tags(html):
         char = html[i]
@@ -19,25 +20,28 @@ def validate_html(html):
                 balanced = False
             else:
                 top = lst.pop()
-                if not _matches(top,char):
-                       balanced = False
+                if not 
     if balanced and lst==[]:
         return True
     else:
         return False
+    """
 
-def _matches(left,right):
-    lefts = "([{"
-    rights = ")]}"
-    return lefts[left] == rights[right]
-    
+    lst = _extract_tags(html)
+    for tag in tags:
+        if tag[1:] not in tags[-1][2:]:
+            return False
+        else:
+            tags = tags[2:-1]
+    return True
+
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
     # then process these html tags using the balanced parentheses algorithm from the book
     # the main difference between your code and the book's code will be that you will have to keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
 
-"""
+
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
@@ -48,6 +52,12 @@ def _extract_tags(html):
     ['<strong>', '</strong>']
     '''
     tags = []
-    for '<' in html:
-        tags =  
-"""
+    while len(html) != 0:
+        left = html.index('<')
+        right = html.index('>')
+
+        tags.append(html[left:(right + 1)])
+        html = html[(right + 1):]
+
+
+
